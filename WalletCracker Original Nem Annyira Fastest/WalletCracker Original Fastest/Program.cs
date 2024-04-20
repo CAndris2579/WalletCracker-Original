340f77474624bead261bd9d1f9174e4028f787a0
CAndris2579 <andrewchren@skiff.com>
2024-04-20 09:19:57 +0200

    Version 0.2.1

--- WalletCracker Original Nem Annyira Fastest/WalletCracker Original Fastest/Program.cs
+++ WalletCracker Original Nem Annyira Fastest/WalletCracker Original Fastest/Program.cs
@@ -38,33 +38,43 @@ class Program
 
     static async Task GenerateWalletsAsync(int tasks)
     {
-        await Task.Run(async () =>
+        while (true)
         {
-            while (true)
+            await Task.Delay(10); // Várakozás
+
+            var tasksList = new List<Task>();
+            var walletsToAdd = new List<WalletInfo>();
+
+            Parallel.For(0, tasks, i =>
             {
-                var tasksList = new List<Task>();
-                for (int i = 0; i < tasks; i++)
+                var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+                ExtKey extendedKey = mnemonic.DeriveExtKey();
+                Key privateKey = extendedKey.PrivateKey;
+                var publicKey = privateKey.PubKey;
+                var address = publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
+                var wallet = new WalletInfo
                 {
-                    tasksList.Add(Task.Run(() =>
-                    {
-                        var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
-                        ExtKey extendedKey = mnemonic.DeriveExtKey();
-                        Key privateKey = extendedKey.PrivateKey;
-                        var publicKey = privateKey.PubKey;
-                        var address = publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
-                        var wallet = new WalletInfo
-                        {
-                            Mnemonic = mnemonic.ToString(),
-                            Address = address,
-                            Balance = 0
-                        };
-                        wallets.Add(wallet);
-                    }));
+                    Mnemonic = mnemonic.ToString(),
+                    Address = address,
+                    Balance = 0
+                };
+
+                lock (wallets)
+                {
+                    walletsToAdd.Add(wallet);
                 }
-                await Task.Delay(50);
-                await Task.WhenAll(tasksList);
+            });
+
+            await Task.WhenAll(tasksList);
+
+            lock (wallets)
+            {
+                foreach (var wallet in walletsToAdd)
+                {
+                    wallets.Add(wallet);
+                }
             }
-        });
+        }
     }
 
     static async Task QueryBalancesAsync(int balanceCheck)
@@ -111,5 +121,4 @@ class Program
             }
         }
     }
-
 }
